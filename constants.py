48696dffa96052612ac3e8f252771034143ce2c0
manaliich <152968747+manaliich@users.noreply.github.com>
2023-12-05 18:31:07 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,49 @@
+SIZE = 400
+GRID_LEN = 4
+GRID_PADDING = 10
+BACKGROUND_COLOR_GAME = "#92877d"
+BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
+
+BACKGROUND_COLOR_DICT = {
+    2: "#FAEBD7",
+    4: "#EEDFCC",
+    8: "#CDC0B0",
+    16: "#8B8378",
+    32: "#7FFFD4",
+    64: "#66CDAA",
+    128: "#F0FFFF",
+    256: "#C1CDCD",
+    512: "#E3CF57",
+    1024: "#FFE4C4",
+    2048: "#000000",
+    4096: "#0000FF",
+    8192: "#0000CD",
+    16384: "#FFEBCD",
+    32768: "#E0EEEE",
+    65536: "#8B7D6B"
+}
+
+CELL_COLOR_DICT = {
+    2: "#F0E68C",
+    4: "#FFE4B5",
+    8: "#AFEEEE",
+    16: "#FFF5EE",
+    32: "#FFDAB9",
+    64: "#ADD8E6",
+    128: "#00FF00",
+    256: "#98FB98",
+    512: "#FFFAF0",
+    1024: "#FFEFD5",
+    2048: "#F0FFF0",
+    4096: "#E6E6FA",
+    8192: "#FFFAFA",
+    16384: "#F0FFF0",
+    32768: "#B0C4DE",
+    65536: "#FFFACD"
+}
+
+FONT = ("VERDANA", 40, "bold")
+KEY_UP = "w"
+KEY_DOWN = "s"
+KEY_LEFT = "a"
+KEY_RIGHT = "d"
